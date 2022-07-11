584338ed1352b00aafdda449d8c89ce92be5c004
Dan Aukes <danaukes@gmail.com>
2022-07-10 21:49:56 -0700

    update

--- python/media_tools/files/flatten_structure.py
+++ python/media_tools/files/flatten_structure.py
@@ -8,20 +8,41 @@ Created on Wed Jun 22 19:10:55 2022
 
 import os
 import shutil
+import argparse
 
-verbose = False
+verbose = True
 dry_run = False
 
-root_path = '/home/danaukes/Desktop/test'
+#root_path = '/home/danaukes/Desktop/test'
 
-for directory, subdirectories, files in list(os.walk(root_path,topdown=True))[1:]:
-    for file in files:
-        file_from = os.path.join(directory,file)
-        if verbose:
-            print('move ', file_from, 'to ', file_to)
-        if not dry_run:
-            try:
-                shutil.move(file_from,root_path)
-            except shutil.Error as e:
-                print(e)
+def flatten(path):
+    for directory, subdirectories, files in list(os.walk(path,topdown=True))[1:]:
+        for file in files:
+            file_from = os.path.join(directory,file)
+            if verbose:
+                print('move ', file_from, 'to ', path)
+            if not dry_run:
+                try:
+                    shutil.move(file_from,path)
+                except shutil.Error as e:
+                    print(e)
         
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('path',metavar='path',type=str,help='path', default = './')
+    # # parser.add_argument('-p','--path',dest='path',default = '*.mp4')
+    # # parser.add_argument('-c','--config',dest='config',default = None)
+    # # parser.add_argument('-f','--filetype',dest='filetypes',default = 'mp4')
+    # # parser.add_argument('-t','--thumbs',dest='thumbs',action='store_true', default = None)
+    # parser.add_argument('-t','--thumb_path',dest='thumb_path',default = None)
+    # parser.add_argument('-v','--video_path',dest='video_path',default = None)
+    # parser.add_argument('-q','--crf',dest='crf',default = None)
+    # parser.add_argument('-p','--preset',dest='preset',default = None)
+    # parser.add_argument('-f','--force',dest='force',action='store_true', default = None)
+    # # parser.add_argument('command',metavar='command',type=str,help='command', default = '')
+    # # parser.add_argument('--token',dest='token',default = None)
+    args = parser.parse_args()
+    # print('path: ',args.path)
+    
+    flatten(args.path)
