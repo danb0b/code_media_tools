916b3f034c73ee2454a68c55c3828767ae7badc8
Dan Aukes <danaukes@danaukes.com>
2022-12-28 21:58:19 -0700

    update

--- python/media_tools/files/flatten_structure.py
+++ python/media_tools/files/flatten_structure.py
@@ -10,12 +10,9 @@ import os
 import shutil
 import argparse
 
-verbose = True
-dry_run = False
-
 #root_path = '/home/danaukes/Desktop/test'
 
-def flatten(path):
+def flatten(path,dry_run=True,verbose=True):
     for directory, subdirectories, files in list(os.walk(path,topdown=True))[1:]:
         for file in files:
             file_from = os.path.join(directory,file)
@@ -36,13 +33,13 @@ if __name__=='__main__':
     # # parser.add_argument('-f','--filetype',dest='filetypes',default = 'mp4')
     # # parser.add_argument('-t','--thumbs',dest='thumbs',action='store_true', default = None)
     # parser.add_argument('-t','--thumb_path',dest='thumb_path',default = None)
-    # parser.add_argument('-v','--video_path',dest='video_path',default = None)
+    parser.add_argument('-v','--verbose',dest='verbose',action='store_true',default = False)
     # parser.add_argument('-q','--crf',dest='crf',default = None)
     # parser.add_argument('-p','--preset',dest='preset',default = None)
-    # parser.add_argument('-f','--force',dest='force',action='store_true', default = None)
+    parser.add_argument('-d','--dry-run',dest='dry_run',action='store_true', default = True)
     # # parser.add_argument('command',metavar='command',type=str,help='command', default = '')
     # # parser.add_argument('--token',dest='token',default = None)
     args = parser.parse_args()
     # print('path: ',args.path)
     
-    flatten(args.path)
+    flatten(args.path,args.dry_run,args.verbose)
