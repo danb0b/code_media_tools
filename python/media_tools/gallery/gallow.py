15d7fa2ee9b9486bdd504f8cb78172a6e57e2687
Dan Aukes <danaukes@danaukes.com>
2022-12-28 23:21:47 -0700

    made it bash friendly

--- python/media_tools/gallery/gallow.py
+++ python/media_tools/gallery/gallow.py
@@ -28,124 +28,152 @@ import subprocess
 size = 1000, 200
 size_non_thumbnail = 1000, 1000
 
-rebuild_from_scratch=False
+#rebuild_from_scratch=False
 # rebuild_from_scratch = True
-rebuild_html_only = False
+#rebuild_html_only = False
 #crf = None
 #crf=21
-crf = 40
+#crf = 40
 #preset = None
 #preset = 'slow'
-preset = 'ultrafast'
-verbose = True
+#preset = 'ultrafast'
+#verbose = True
+
+import os
+
+def mywalk(top, maxdepth):
+    dirs, nondirs = [], []
+    for entry in os.scandir(top):
+        (dirs if entry.is_dir() else nondirs).append(entry.path)
+    yield top, dirs, nondirs
+    if maxdepth > 1:
+        for path in dirs:
+            for x in mywalk(path, maxdepth-1):
+                yield x
+
 
 
 def fix(input):
     return os.path.normpath(os.path.expanduser(input))
 
 
-source_root = fix('/cloud/drive_asu_idealab/videos')
-# source_root = fix('/storage/nas/photos/2022')
-# source_root = fix('~/cloud/drive_stanford/library/videos')
+def build(source_root,gallery_root,crf=40,preset='ultrafast',rebuild_from_scratch=True,rebuild_html_only=False,verbose=True,max_depth=100):
 
-gallery_root = fix('~/Desktop/gallery')
-# gallery_root = fix('~/Desktop/2022')
-# gallery_root = fix('/home/danaukes/Desktop/library_videos')
+    #dry_run=True
 
-if rebuild_from_scratch:
-    if os.path.exists(gallery_root):
-        shutil.rmtree(gallery_root)
+    #source_root = fix('/cloud/drive_asu_idealab/videos')
+    # source_root = fix('/storage/nas/photos/2022')
+    # source_root = fix('~/cloud/drive_stanford/library/videos')
 
-for folder, subfolders, files in os.walk(source_root):
-    images = [item for item in files if (os.path.splitext(
-        item)[1][1:]).lower() in media_tools.image_filetypes]
-    videos = [item for item in files if (os.path.splitext(
-        item)[1][1:]).lower() in media_tools.video_filetypes]
+    #gallery_root = fix('~/Desktop/gallery')
+    # gallery_root = fix('~/Desktop/2022')
+    # gallery_root = fix('/home/danaukes/Desktop/library_videos')
 
-    if verbose:
-        print(yaml.dump(images))
-        print(yaml.dump(videos))
+    crf = float(crf)
+    max_depth = int(max_depth)
+    
+    source_root = fix(source_root)
+    gallery_root = fix(gallery_root)
 
-    to_strip = os.path.commonpath([source_root, folder])
-    elements = to_strip.split(os.path.sep)
-    tail = folder.split(os.path.sep)[len(elements):]
-    newfolder = os.path.join(gallery_root, *tail)
-    try:
-        os.makedirs(newfolder)
-    except FileExistsError:
-        if rebuild_from_scratch:
-            raise
-        else:
-            pass
+    if rebuild_from_scratch:
+        if os.path.exists(gallery_root):
+            shutil.rmtree(gallery_root)
 
-    #images = [item for item in files if os.path.splitext(item)[1][1:] in image_filetypes]
-    #videos = [item for item in files if os.path.splitext(item)[1][1:] in video_filetypes]
+    for folder, subfolders, files in mywalk(source_root,max_depth):
+        images = [item for item in files if (os.path.splitext(
+            item)[1][1:]).lower() in media_tools.image_filetypes]
+        videos = [item for item in files if (os.path.splitext(
+            item)[1][1:]).lower() in media_tools.video_filetypes]
 
-    #print(yaml.dump(images))
-    #print(yaml.dump(videos))
+        if verbose:
+            print(yaml.dump(images))
+            print(yaml.dump(videos))
 
-    markdown_file_path = os.path.join(newfolder, 'index.md')
-    html_file_path = os.path.join(newfolder, 'index.html')
+        to_strip = os.path.commonpath([source_root, folder])
+        elements = to_strip.split(os.path.sep)
+        tail = folder.split(os.path.sep)[len(elements):]
+        newfolder = os.path.join(gallery_root, *tail)
+        try:
+            os.makedirs(newfolder)
+        except FileExistsError:
+            if rebuild_from_scratch:
+                raise
+            else:
+                pass
 
-    s = ''
-    s += '## Subfolders\n\n'
-    for item in subfolders:
-        s += '* [{0}]({0}/index.html)\n'.format(item)
-    s += '\n## Pictures\n\n'
-    for item in images:
-        a, b = os.path.splitext(item)
-        s += '[![]({0})]({1}) '.format(a+'_thumb'+b, item)
-    s += '\n\n## Videos\n\n'
-    for item in videos:
-        a, b = os.path.splitext(item)
-        thumb = a+'_thumb.png'
-        vid = a+'.mp4'
-        s += '[![]({0})]({1}) '.format(thumb, vid)
-    with open(markdown_file_path, 'w') as f:
-        f.write(s)
+        #images = [item for item in files if os.path.splitext(item)[1][1:] in image_filetypes]
+        #videos = [item for item in files if os.path.splitext(item)[1][1:] in video_filetypes]
 
-    subprocess.run('pandoc "{0}" -o "{1}"'.format(markdown_file_path,
-                   html_file_path), capture_output=True, shell=True)
+        #print(yaml.dump(images))
+        #print(yaml.dump(videos))
 
-    # to_strip = os.path.normpath(to_strip).split(os.path.sep)
+        markdown_file_path = os.path.join(newfolder, 'index.md')
+        html_file_path = os.path.join(newfolder, 'index.html')
 
-    # os.makedirs()
-
-    bad_photos = []
-    jj_last = 0
-    if not rebuild_html_only:
-        for ii, item in enumerate(images):
-            jj = (round(ii/len(images)*100))
-            if jj > jj_last:
-                print('\r', jj, end="")
-
-            support.process_image(item, folder, newfolder,
-                                  size, size_non_thumbnail, bad_photos)
-            jj_last = jj
-
+        s = ''
+        s += '## Subfolders\n\n'
+        for item in subfolders:
+            s += '* [{0}]({0}/index.html)\n'.format(item)
+        s += '\n## Pictures\n\n'
+        for item in images:
+            a, b = os.path.splitext(item)
+            s += '[![]({0})]({1}) '.format(a+'_thumb'+b, item)
+        s += '\n\n## Videos\n\n'
         for item in videos:
-            if verbose:
-                print('process video', item)
-            support.process_video(item, folder, newfolder,crf, preset, rebuild_from_scratch,verbose=False,size=size)
+            a, b = os.path.splitext(item)
+            thumb = a+'_thumb.png'
+            vid = a+'.mp4'
+            s += '[![]({0})]({1}) '.format(thumb, vid)
+        with open(markdown_file_path, 'w') as f:
+            f.write(s)
 
-    #     # i.show()
-    #     # display(i)
+        subprocess.run('pandoc "{0}" -o "{1}"'.format(markdown_file_path,
+                    html_file_path), capture_output=True, shell=True)
+
+        # to_strip = os.path.normpath(to_strip).split(os.path.sep)
+
+        # os.makedirs()
+
+        bad_photos = []
+        jj_last = 0
+        if not rebuild_html_only:
+            for ii, item in enumerate(images):
+                jj = (round(ii/len(images)*100))
+                if jj > jj_last:
+                    print('\r', jj, end="")
+
+                support.process_image(item, folder, newfolder,
+                                    size, size_non_thumbnail, bad_photos)
+                jj_last = jj
+
+            for item in videos:
+                if verbose:
+                    print('process video', item)
+                support.process_video(item, folder, newfolder,crf, preset, rebuild_from_scratch,verbose=False,size=size)
+
+        #     # i.show()
+        #     # display(i)
 if __name__ == '__main__':
 
-    pass
-    # parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-    # parser.add_argument('path',metavar='path',type=str,help='path', default = '*.mp4')
+    #parser.add_argument('path',metavar='path',type=str,help='path', default = '*.mp4')
     # # parser.add_argument('-p','--path',dest='path',default = '*.mp4')
     # # parser.add_argument('-c','--config',dest='config',default = None)
     # # parser.add_argument('-f','--filetype',dest='filetypes',default = 'mp4')
     # # parser.add_argument('-t','--thumbs',dest='thumbs',action='store_true', default = None)
-    # parser.add_argument('-t','--thumb_path',dest='thumb_path',default = None)
+    parser.add_argument('-i','--input-path',dest='input_path',default = None)
+    parser.add_argument('-o','--output-path',dest='output_path',default = None)
     # parser.add_argument('-v','--video_path',dest='video_path',default = None)
-    # parser.add_argument('-q','--crf',dest='crf',default = None)
-    # parser.add_argument('-p','--preset',dest='preset',default = None)
-    # parser.add_argument('-f','--force',dest='force',action='store_true', default = None)
+    parser.add_argument('-q','--crf',dest='crf',default = '40')
+    parser.add_argument('-p','--preset',dest='preset',default = 'ultrafast')
+    parser.add_argument('-s','--scratch',dest='scratch',help="rebuild from scratch",action='store_true', default = False)
+    parser.add_argument('--html',dest='html',help="rebuild html only",action='store_true', default = False)
+    #parser.add_argument('-d','--dry-run',dest='dry_run',help="dry run",action='store_true', default = False)
+    parser.add_argument('-v','--verbose',dest='verbose',help="verbose",action='store_true', default = False)
+    parser.add_argument('-d','--max-depth',dest='max_depth',help="max depth", default = 100)
     # # parser.add_argument('command',metavar='command',type=str,help='command', default = '')
     # # parser.add_argument('--token',dest='token',default = None)
-    # args = parser.parse_args()
+    args = parser.parse_args()
     # print('path: ',args.path)
+    build(args.input_path,args.output_path,args.crf,args.preset,args.scratch,args.html,args.verbose,args.max_depth)
