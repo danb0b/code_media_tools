bf99faba4008fddd81c477ee3860647997c6ad69
Dan Aukes <danaukes@gmail.com>
2023-12-01 22:43:22 +0300

    update

--- python/media_tools/__init__.py
+++ python/media_tools/__init__.py
@@ -7,5 +7,5 @@ Created on Fri Aug 27 11:17:26 2021
 """
 
 image_filetypes = ['jpg','jpeg','png','gif']
-video_filetypes = ['mov','mp4','avi','mkv','webm','mpg','wmv','3gpp','ogg']
+video_filetypes = ['mov','mp4','avi','mkv','webm','mpg','wmv','3gpp','ogg','gif']
 yaml_filetypes = ['yaml','yml']
